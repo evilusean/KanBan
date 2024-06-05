c3c5b6577db06424dcd3360ac985e673c9cc1837
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-05 14:38:43 -0300

    Update tailwind.config.js
    added colors

--- kanban/tailwind.config.js
+++ kanban/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBackgroundColor: "#001117",
+        columnBackgroundColor: "$161C22",
+      },
+    },
   },
   plugins: [],
 };
