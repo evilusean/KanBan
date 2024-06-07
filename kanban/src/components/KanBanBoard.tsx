e48bd6e35f5d363092594782077618ab3769d326
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-07 16:19:39 -0300

    Update KanBanBoard.tsx

--- kanban/src/components/KanBanBoard.tsx
+++ kanban/src/components/KanBanBoard.tsx
@@ -2,9 +2,21 @@ import React from "react";
 
 function KanBanBoard() {
   return (
-    <div>
-      <button
-        className="
+    <div
+      className="
+    m-auto
+    flexmin-h-screen
+    w-full
+    items-center
+    overflow-x-auto
+    overflow-y-hidden
+    px-[40px]
+
+    "
+    >
+      <div className="m-auto">
+        <button
+          className="
       h-[60px] 
       w-[350px] 
       min-w-[350px] 
@@ -16,9 +28,10 @@ function KanBanBoard() {
       p-4 ring-rose-500 
       hover:ring-2
       "
-      >
-        Add Column
-      </button>
+        >
+          Add Column
+        </button>
+      </div>
     </div>
   );
 }
