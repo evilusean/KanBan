b6958899811052426a1a84f36f48a582ac607b7a
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-07 19:55:22 -0300

    Create ColumnContainer.tsx

---
+++ kanban/src/components/ColumnContainer.tsx
@@ -0,0 +1,11 @@
+import { Column } from "../types";
+
+interface Props {
+  column: Column;
+}
+function ColumnContainer(props: Props) {
+  const { column } = props;
+  return <div>{column.title}</div>;
+}
+
+export default ColumnContainer;
