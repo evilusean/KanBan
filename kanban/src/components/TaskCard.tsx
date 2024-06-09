e6bd6c2003d900dbc6ce04a36571402a352332e9
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-09 18:31:32 -0300

    Update TaskCard.tsx
    edit updateTask

--- kanban/src/components/TaskCard.tsx
+++ kanban/src/components/TaskCard.tsx
@@ -5,14 +5,54 @@ import { Id, Task } from "../types";
 interface Props {
   task: Task;
   deleteTask: (id: Id) => void;
+  updateTask: (id: Id, content: string) => void;
 }
 
 function TaskCard({ task, deleteTask }: Props) {
   const [mouseIsOver, setMouseIsOver] = useState(false);
   const [editMode, setEditMode] = useState(false);
 
+  const toggleEditMode = () => {
+    setEditMode((prev) => !prev);
+    setMouseIsOver(false);
+  };
+
+  if (editMode) {
+    return (
+      <div
+        className="bg-mainBackgroundColor 
+  p-2.5 
+  h-[100px] 
+  min-h-[100px] 
+  items-center 
+  flex 
+  text-left 
+  rounded-xl 
+  hover:ring-2 
+  hover:ring-inset 
+  hover:ring-rose-500 
+  cursor-grab
+  relative"
+      >
+        <textarea
+          className="h-90% w-full
+        border-none rounded bg-transparent text-white focus:outline-none"
+          value={task.content}
+          autoFocus
+          placeholder="Task content here"
+          onBlur={toggleEditMode}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") toggleEditMode();
+          }}
+          onChange={(e) => updateTask(task.id, e.target.value)}
+        ></textarea>
+      </div>
+    );
+  }
+
   return (
     <div
+      onClick={toggleEditMode}
       className="bg-mainBackgroundColor 
     p-2.5 
     h-[100px] 
@@ -24,7 +64,7 @@ function TaskCard({ task, deleteTask }: Props) {
     hover:ring-2 
     hover:ring-inset 
     hover:ring-rose-500 
-    cursor-grabs
+    cursor-grab
     relative"
       onMouseEnter={() => {
         setMouseIsOver(true);
